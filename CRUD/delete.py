46897e97d1f2a4985fa7d4557b155723ce1f9302
modernsege <68877704+modernsege@users.noreply.github.com>
2021-05-16 18:50:01 +0200

    Modified delete.py in CRUD
    delete_rows method was added so deleting rows in CRUD works

--- CRUD/delete.py
+++ CRUD/delete.py
@@ -1,8 +1,20 @@
 import mysql.connector
 from raw_crud import delete_sth
-
+from raw_crud import display_tables
+from raw_crud import if_string_to_SQL
 
 class Delete:
     def delete_rows(self, conn):
+        all_tables = display_tables(conn)
 
-        delete_sth()
+        for i in range(len(all_tables)):
+            print(all_tables[i].upper(), end=" | ")
+
+        chosen_table = input("\nIn which table you want to delete a rows? Choose one: ")
+
+        if chosen_table in all_tables:
+            where_condition = input("Insert where condition(SQL format):")
+
+
+
+        delete_sth(conn, chosen_table, where_condition)
